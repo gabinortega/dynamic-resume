b5dfb60e5b80aa5d4d16ba756f072320e84ed46c
Gabino <gabinortega@gmail.com>
2023-04-12 01:03:41 -0400

    Adding start page
    Create  start page component, adding routes, remove angular scaffolding

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InitComponent } from './init/init.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'init',
+    component: InitComponent,
+  },
+  {
+    path: '',
+    component: InitComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}

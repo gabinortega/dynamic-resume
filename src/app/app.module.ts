7266d750eaba7685a9337fbded490b795a1b9bdd
Gabino <gabinortega@gmail.com>
2023-04-12 01:32:12 -0400

    Adding primeng
    Adding primeng, primeicons and primeflex

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ButtonModule } from 'primeng/button';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { InitComponent } from './init/init.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    InitComponent
-  ],
+  declarations: [AppComponent, InitComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    ButtonModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}

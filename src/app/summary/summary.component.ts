9cd03450a46cd190761209bdbde390a4795be21b
DerekSave <123384462+DerekSave@users.noreply.github.com>
2023-05-02 11:03:51 -0400

    Integrated buttons of add job and add study

--- src/app/summary/summary.component.ts
+++ src/app/summary/summary.component.ts
@@ -1,8 +1,16 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-summary',
   templateUrl: './summary.component.html',
   styleUrls: ['./summary.component.scss'],
 })
-export class SummaryComponent {}
+export class SummaryComponent {
+  formGroup: FormGroup = new FormGroup({});
+  ngOnInit(): void {
+    this.formGroup = new FormGroup({
+      sumary: new FormControl<string | null>(null),
+    });
+  }
+}
